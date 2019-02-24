92a3a9e88631f97818a7411103d30dfebc415acd
tuandaodev <tuandao.dev@gmail.com>
2019-02-24 17:11:41 +0700

    Update Getid.php

--- application/controllers/api/Getid.php
+++ application/controllers/api/Getid.php
@@ -142,6 +142,7 @@ class Getid extends CI_Controller {
                 $response['result'] = 0;
 
                 $inbox_url = trim($get_data_inbox_url['link']);
+                $inbox_url = unicode_decode($inbox_url);
 
                 $inbox_url = "https://en-gb.facebook.com/" . $inbox_url;
 
@@ -347,4 +348,11 @@ function array_to_xml($array, &$user_message) {
             $user_message->addChild("$key",htmlspecialchars("$value"));
         }
     }
+}
+
+function replace_unicode_escape_sequence($match) {
+    return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
+}
+function unicode_decode($str) {
+    return preg_replace_callback('/\\\\u([0-9a-f]{4})/i', 'replace_unicode_escape_sequence', $str);
 }
